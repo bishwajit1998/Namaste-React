c81c33483048c1920f29f621319e762751866181
bishwajit <38694064+bishwajit1998@users.noreply.github.com>
2023-10-12 06:29:42 +0530

    adding hooks to the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
+          <li>Home </li>
           <li>About Us </li>
           <li>Contact Us</li>
           <li>Cart</li>
